445e7af12637665dc2f1dd6eea7cc208f4b1919b
Kortney Canyon <kcanyon_2000@yahoo.com>
2023-01-18 09:51:40 -0600

    Initial Users and table created

--- db/init_db.js
+++ db/init_db.js
@@ -4,29 +4,69 @@ const {
   // for example, User
 } = require('./');
 
-async function buildTables() {
+// drop tables in correct order
+async function dropTables() {
+  console.log("Dropping All Tables...")
   try {
-    client.connect();
-
-    // drop tables in correct order
-
-    // build tables in correct order
+    await client.query(`
+  DROP TABLE IF EXISTS users;
+  `);
   } catch (error) {
+    console.error("Error dropping tables!");
     throw error;
   }
+  console.log("All Tables Dropped!")
 }
+
+// create all tables, in the correct order
+async function createTables() {
+  console.log("Starting to build tables...")
+  try {
+    await client.query(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL
+    `);
+  } catch (error) {
+    console.error("Error building tables!");
+    throw error;
+  }
+  console.log("All Tables created!")
+};
 
 async function populateInitialData() {
   try {
     // create useful starting data by leveraging your
     // Model.method() adapters to seed your db, for example:
     // const user1 = await User.createUser({ ...user info goes here... })
+    const usersToCreate = [
+      { username: "albert", password: "bertie99" },
+      { username: "sandra", password: "sandra123" },
+      { username: "glamgal", password: "glamgal123" },
+    ];
+    const users = await Promise.all(usersToCreate.map(createUser))
+
+    console.log('Users created');
+    console.log(users);
+    console.log("Finished creating users!")
   } catch (error) {
     throw error;
   }
 }
 
-buildTables()
+async function rebuildDB() {
+  try {
+    await dropTables()
+    await createTables()
+  } catch (error) {
+    console.log("Error during rebuildDB")
+    throw error
+  }
+}
+
+
+rebuildDB()
   .then(populateInitialData)
   .catch(console.error)
   .finally(() => client.end());
