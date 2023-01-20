93e9480bef0b361521af30eb4571f7086274a736
David Giwa <david.s.giwa@gmail.com>
2023-01-20 13:40:25 -0600

    fixed??????

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { App } from './components';
 // place them in the src/style directory, and import them like this:
 import "./style/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
