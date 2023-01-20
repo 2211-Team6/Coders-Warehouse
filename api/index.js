92dd85362bb7285e6f8efce3d066bc9a07100ce6
Brandon Norris <bsn2qg@virginia.edu>
2023-01-20 13:10:01 -0500

    added products and reviews router

--- api/index.js
+++ api/index.js
@@ -20,5 +20,13 @@ apiRouter.use('/user', usersRouter);
 const cartRouter = require('./cart');
 apiRouter.use('./cart', cartRouter);
 
+// ROUTER: /api/products
+const productsRouter = require('./products');
+apiRouter.use('/products', productsRouter);
+
+// ROUTER: /api/reviews
+const reviewsRouter = require('./reviews');
+apiRouter.use('/reviews', reviewsRouter);
+
 
 module.exports = apiRouter;
