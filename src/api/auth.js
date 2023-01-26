efc7ab4549fce89be48e5196dbc52a802df8f72e
nick <nicholasdimaggio35@gmail.com>
2023-01-26 12:47:29 -0800

    this is a commit

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,257 +1,241 @@
-export const registerUser = async (username, password) => {
-    try {
-      const response = await fetch("/api/users/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username,
-          password,
-        }),
-      });
-      const result = await response.json();
-      return result.token;
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  export const login = async (username, password) => {
-    try {
-      const verify = await fetch(`/api/users/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, password }),
-      });
-      const data = await verify.json();
-      console.log(data);
-      // right here put the returned data.token into localStorage so that we can use it across our app.
-      localStorage.setItem("token", data.token);
-      localStorage.setItem("username", data.user.username);
-      return data;
-    } catch (error) {
-      console.error(error);
-    }
-  };
+export const registerUser = async (username, password, email) => {
+  try {
+    console.log(username, password, email);
+    const response = await fetch("/api/users/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        email,
+      }),
+    });
+    const result = await response.json();
+    console.log(result);
+    return result.token;
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const login = async (username, password) => {
+  try {
+    const verify = await fetch(`/api/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    const data = await verify.json();
+    console.log(data);
+    // right here put the returned data.token into localStorage so that we can use it across our app.
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("username", data.user.username);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 //************ PRODUCTS ************//
 
 export const getAllProducts = async () => {
-    try {
-      const response = await fetch(
-        "/api/products",
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const result = await response.json();
-      return result;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  try {
+    const response = await fetch("/api/products", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+export const getProductById = async (id) => {
+  try {
+    const response = await fetch(`/api/products/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  export const getProductById = async (id) => {
-    try {
-      const response = await fetch(
-        `/api/products/${id}`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const result = await response.json();
-      return result;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+export const createProduct = async (
+  token,
+  title,
+  description,
+  price,
+  quantity
+) => {
+  try {
+    const response = await fetch("/api/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        price,
+        quantity,
+      }),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+export const updateProduct = async (
+  token,
+  title,
+  description,
+  price,
+  quantity,
+  id
+) => {
+  try {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        price,
+        quantity,
+      }),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  export const createProduct = async (token, title, description, price, quantity) => {
-    try {
-      const response = await fetch(
-        "/api/products",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify({
-            title,
-            description,
-            price,
-            quantity,
-          }),
-        }
-      );
-      const result = await response.json();
-      return result;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-
-  export const updateProduct = async (token, title, description, price, quantity, id) => {
-    try {
-      const response = await fetch(
-        `/api/products/${id}`,
-        {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify({
-            title,
-            description,
-            price,
-            quantity,
-          }),
-        }
-      );
-      const result = await response.json();
-      return result;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-
-  export const deleteProduct = async (token, productId) => {
-    try {
-      const response = await fetch(
-        `/api/products/${productId}`,
-        {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      const result = await response.json();
-      return result;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-//************ REVIEWS ************//
-
-export const addReview = async (userName, productId, rating, description) => {
-    const token = localStorage.getItem("token");
-    try {
-      const response = await fetch(
-        "/api/reviews",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify({
-            userName,
-            productId,
-            rating,
-            description
-          }),
-        }
-      );
-      const result = await response.json();
-    //   console.log("whyyyyyyyyyyyyy", result);
-      return result;
-    } catch (error) {
-      throw error;
-    }
-  };
-
-  
-export const getReviews = async () => {
-    try {
-      const response = await fetch(
-        "/api/reviews"
-      );
-      const data = await response.json();
-      // console.log(data);
-      return data;
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-
-  export async function deleteReview(id, token) {
-    const response = await fetch(`/api/reviews/${id}`, {
+export const deleteProduct = async (token, productId) => {
+  try {
+    const response = await fetch(`/api/products/${productId}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
     });
-    return await response.json();
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
   }
+};
 
+//************ REVIEWS ************//
 
-  //************ CART ************//
+export const addReview = async (userName, productId, rating, description) => {
+  const token = localStorage.getItem("token");
+  try {
+    const response = await fetch("/api/reviews", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        userName,
+        productId,
+        rating,
+        description,
+      }),
+    });
+    const result = await response.json();
+    //   console.log("whyyyyyyyyyyyyy", result);
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
 
-  export async function fetchCartProducts(cartProduct) {
-     try {
-      const response = await fetch(
-        "/api/cart",
-        {
-          headers: {
-            "Content-Type": "application/json",
-            body: JSON.stringify(cartProduct),
-          },
-        }
-      );
-      const result = await response.json();
-      return result;
-    } catch (error) {
-      // console.log(error);
-    }
-  };
+export const getReviews = async () => {
+  try {
+    const response = await fetch("/api/reviews");
+    const data = await response.json();
+    // console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-  export async function updateCartProduct(cartProduct){
-    try {
-      const response = await fetch(`/api/cart/${cartProduct.id}`, {
-        method: "PATCH",
-        headers: { "Content-Type": "application/json" },
+export async function deleteReview(id, token) {
+  const response = await fetch(`/api/reviews/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return await response.json();
+}
+
+//************ CART ************//
+
+export async function fetchCartProducts(cartProduct) {
+  try {
+    const response = await fetch("/api/cart", {
+      headers: {
+        "Content-Type": "application/json",
         body: JSON.stringify(cartProduct),
-      });
-      if(response.ok) {
-        fetchcartProducts();
-      } else {
-        console.log(`Error: ${response.status}`);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+      },
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    // console.log(error);
+  }
+}
 
-  export async function removeCartProduct(cartProductId){
-    try {
-      const response = await fetch(`/api/cart/${cartProductId}`, {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-      });
-      if(response.ok) {
-        fetchcartProducts();
-      } else {
-        console.log(`Error: ${response.status}`);
-      }
-    } catch (error) {
-      console.log(error);
+export async function updateCartProduct(cartProduct) {
+  try {
+    const response = await fetch(`/api/cart/${cartProduct.id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(cartProduct),
+    });
+    if (response.ok) {
+      fetchcartProducts();
+    } else {
+      console.log(`Error: ${response.status}`);
     }
-  };
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-  
+export async function removeCartProduct(cartProductId) {
+  try {
+    const response = await fetch(`/api/cart/${cartProductId}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      fetchcartProducts();
+    } else {
+      console.log(`Error: ${response.status}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import { getAPIHealth } from "../axios-services";
+
 import "../style/App.css";
 import Register from "./Register.js";
 import Login from "./Login.js";
-import Cart from "./Cart"
+import Cart from "./Cart";
 import ReviewForm from "./ReviewForm";
 
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem("token"));
-  const [APIHealth, setAPIHealth] = useState("");
 
-  useEffect(() => {
-    const getAPIStatus = async () => {
-      const { healthy } = await getAPIHealth();
-      setAPIHealth(healthy ? "api is up! :D" : "api is down :/");
-    };
-    getAPIStatus();
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div className="app-container">
       <h1>Hello, World!</h1>
-      <p>API Status: {APIHealth}</p>
+
       <Routes>
         <Route path="/" element={<Home token={token} setToken={setToken} />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/review-form" element={<ReviewForm />} />
-        <Route path="/cart" element={<Cart/>} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
   );
