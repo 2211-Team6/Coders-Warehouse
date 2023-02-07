13eb0f32241101586b2719b7f8b8fdef737e4fa9
Kortney Canyon <kcanyon_2000@yahoo.com>
2023-02-07 14:33:27 -0600

    star rating system connected

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import Star from "./Star";
 
+
 const createArray = (length) => [...Array(length)];
 
-const StarRating = ({totalStars}) => {
+const StarRating = ({totalStars, setRating, rating}) => {
     const [selectedStars, setSelectedStars] = useState(0)
-    console.log("this is selected stars: ", selectedStars)
+    console.log("this is selected stars: ", rating)
     return <>
     {createArray(totalStars).map((n, index) => {
         return (
-        <Star 
+        <Star
         key={index} 
-        selected={selectedStars > index} 
-        onSelect={() => setSelectedStars(index + 1)}
+        selected={rating > index} 
+        onSelect={() => setRating(index + 1)}
         />
         )  
     })}
